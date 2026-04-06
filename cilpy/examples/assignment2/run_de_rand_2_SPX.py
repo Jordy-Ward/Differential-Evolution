1903b5935acd36f75642da2d1cfff4082bc604b2
Jordy Michael Ward <jordyward041@gmail.com>
2026-04-06 16:09:02 +0200

    feature(Add exp scripts to benchmark 5 funcs)
    Multi modal, constrained functions. 30 runs, 3000 iterations. For all
    de's.

---
+++ cilpy/examples/assignment2/run_de_rand_2_SPX.py
@@ -0,0 +1,38 @@
+import sys
+import os
+
+from cilpy.problem.de_benchmark_funcs import Rastrigin, Ackley, Griewank, Schwefel, Michalewicz
+from cilpy.solver.de.de_rand_2_SPX import DE_SPX
+from cilpy.runner import ExperimentRunner
+
+def main():
+    problems = [
+        Rastrigin(dimension=30),
+        Ackley(dimension=30),
+        Griewank(dimension=30),
+        Schwefel(dimension=30),
+        Michalewicz(dimension=30)
+    ]
+
+    solver_configs = [
+        {
+            "class": DE_SPX,
+            "params": {
+                "name": "DE_rand_2_SPX",
+                "population_size": 100,
+                "crossover_rate": 0.8,
+                "f_weight": 0.5,
+            }
+        }
+    ]
+
+    runner = ExperimentRunner(
+        problems=problems,
+        solver_configurations=solver_configs,
+        num_runs=30,
+        max_iterations=3000
+    )
+    runner.run_experiments()
+
+if __name__ == "__main__":
+    main()
